1dc56314686255d75d8c637e1b8e9978467314c1
dessolat <desola2@inbox.ru>
2021-10-06 23:36:47 +0300

    Add navigation, add styles

--- src/App.js
+++ src/App.js
@@ -6,19 +6,43 @@ import Game from './components/Game';
 function App() {
   const [games, setGames] = useState([]);
   const [page, setPage] = useState(1);
+  const [maxPages, setMaxPages] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get('http://localhost:3001');
+      let response = await axios.get('http://localhost:3001/games/count');
+      setMaxPages(Math.ceil(response.data / 8));
+      response = await axios.get(`http://localhost:3001/${page}`);
       setGames(response.data);
     };
     fetchData();
-  }, []);
+  }, [page]);
+
+  const handlePrevClick = () => {
+    if (page <= 1) return;
+    setPage(prev => prev - 1);
+  };
+
+  const handleNextClick = () => {
+    if (page >= maxPages) return;
+    setPage(prev => prev + 1);
+  };
 
   return (
     <div className='container'>
+      <nav>
+        <button className='nav-btn btn' onClick={handlePrevClick} disabled={page <= 1 && true}>
+          {'<'}
+        </button>
+        <button
+          className='nav-btn btn'
+          onClick={handleNextClick}
+          disabled={page >= maxPages && true}>
+          {'>'}
+        </button>
+      </nav>
       <div className='outer-wrapper'>
-        {games.slice((page - 1) * 8, page * 8).map(game => (
+        {games.map(game => (
           <Game key={game.id} game={game} />
         ))}
       </div>
