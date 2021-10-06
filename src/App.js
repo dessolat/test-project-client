cd27276a1ad62c7736f2a7ee657964a1dc568d9f
dessolat <desola2@inbox.ru>
2021-10-06 16:29:02 +0300

    Add axios, create initial request to server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('http://localhost:3001');
+      setGames(response.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
-      
+      {games.map(game => (
+        <p>
+          {game.id}
+        </p>
+      ))}
     </div>
   );
 }
